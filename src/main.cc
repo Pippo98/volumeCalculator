6265e3407fc2693556143917c1bf3dae2b8769e1
Filippo <pippo.faccini@gmail.com>
2020-05-17 17:54:32 +0200

    Hello World of pangolin, basic makefile

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include "pangolin_viewer/viewer.h"
+
+#include <iostream>
+using namespace std;
+
+int main(int, char **)
+{
+    pangolin_viewer::Viewer pgV;
+
+    cout << "uvfdsnkvp\n\n", pgV;
+
+    pgV.canvas();
+
+    return 0;
+}
---
+++ src/pangolin_viewer/viewer.h
@@ -0,0 +1,12 @@
+#ifndef PANGILIN_VIEWER_VIEWER_H
+#define PANGILIN_VIEWER_VIEWER_H
+
+namespace pangolin_viewer
+{
+    class Viewer
+    {
+    public:
+        void canvas();
+    };
+}; // namespace pangolin_viewer
+#endif
